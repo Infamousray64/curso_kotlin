8bf484e2b044aaa37eb73ab1c32d05d6ee38d25a
infamousray64 <ramonmarronr@gmail.com>
2024-06-27 16:47:27 -0400

    Ejercicio1 de google listo

--- app/src/main/java/com/example/myapplication/exercises/Exercise1.kt
+++ app/src/main/java/com/example/myapplication/exercises/Exercise1.kt
@@ -1,2 +1,39 @@
 package com.example.myapplication.exercises
 
+/*
+En el código inicial que se proporciona en el siguiente fragmento de código,
+escribe un programa que imprima el mensaje de resumen según la cantidad de
+notificaciones que recibiste. El mensaje debe incluir lo siguiente:
+
+Resultado:
+- La cantidad exacta de notificaciones cuando haya menos de 100
+- 99+ como cantidad de notificaciones cuando haya 100 o más
+*/
+
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 101
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+/*fun printNotificationSummary(numberOfMessages: Int) {
+    // Fill in the code.
+    if (numberOfMessages < 100) {
+        println("Tienes $numberOfMessages notificaciones")
+    } else {
+        println("Tienes 99+ notificaciones")
+    }
+} */
+
+ // tambien puede ser asi
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    // Fill in the code.
+    if (numberOfMessages <= 100) {
+        println("Tienes $numberOfMessages notificaciones")
+    } else {
+        println("Tienes +99 notificaciones")
+    }
+}
