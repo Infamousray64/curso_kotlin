7b7fdc868d86e8c61d45a51f33dc51a35149bd73
infamousray64 <ramonmarronr@gmail.com>
2024-07-01 11:01:56 -0400

    clase de list

---
+++ app/src/main/java/com/example/myapplication/List.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication
+
+fun main() {
+
+    //inmutableList()
+    mutableList()
+
+}
+
+fun mutableList() {
+
+    val weekDays: MutableList<String> =
+        mutableListOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    weekDays.add(3, "New Day")
+    println(weekDays)
+
+    if (weekDays.isEmpty()) {
+        //no imprime nada
+    } else {
+        weekDays.forEach { println(it) }
+    }
+
+    if (weekDays.isNotEmpty()) {
+        weekDays.forEach { println(it) }
+
+    }
+
+    fun inmutableList() {
+        val readOnly: List<String> =
+            listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+        //println(readOnly.size)
+        //println(readOnly)
+        //println(readOnly.last())
+        //println(readOnly.first())
+
+        //filtrar
+
+        //val example = readOnly.filter { it.length > 6 }
+        //println(example)
+
+        //val example = readOnly.filter { it.contains("s") }
+        //println(example)
+
+        //iterar
+        //readOnly.forEach { weekDay -> println(weekDay) }
+
+    }
